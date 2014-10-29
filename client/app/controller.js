94d3927caa2e660e01989a00962fed37b179e915
Arash Khosravi <arashsoft@gmail.com>
2014-10-29 12:40:07 -0400

    Use bootstrap ui to create components of main-templates

--- client/app/controller.js
+++ client/app/controller.js
@@ -1,18 +1,29 @@
 angular.module('sciencePriorities2App')
-	.controller('MainCtrl', ['$scope', '$stateParams', '$state', function(sc){
+	.controller('MainCtrl', ['$scope', '$stateParams', '$state', function($scope){
 	
-	 sc.benchmarkingEntities=[
-		{"name":"Awards",
-		"properties":["Departments","Sponsors-Programs","Source of funding"],},
-		{"name":"Professors",
-		"properties":["Research chair","Department","Rank"]},
-		{"name":"Publications",
-		"properties":["Type","Department"]},	
-		{"name":"Students",
-		"properties":["Academic Level","Department"]
+	 $scope.benchmarkingEntities=[
+		{
+			"name":"Awards",
+			"properties":["Departments","Sponsors-Programs","Source of funding"],
+			"isOpen":"true"
+		},		
+		{
+			"name":"Professors",
+			"properties":["Research chair","Department","Rank"],
+			"isOpen":"true"
+		},
+		{
+			"name":"Publications",
+			"properties":["Type","Department"],
+			"isOpen":"true"
+		},	
+		{
+			"name":"Students",
+			"properties":["Academic Level","Department"],
+			"isOpen":"true"
 		}
 	 ];
-	 
+
 }]);
 
 angular.module('sciencePriorities2App').controller("CenterController" , ["$scope",function($scope){
