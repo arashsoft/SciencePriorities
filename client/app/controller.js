ec609e6bdf18f199762942710eaac813603ebc95
Arash Khosravi <arashsoft@gmail.com>
2014-11-03 17:38:15 -0500

    Add "select benchmark" on drag&drop - now everything is ready to load d3 with real data

--- client/app/controller.js
+++ client/app/controller.js
@@ -26,11 +26,36 @@ angular.module('sciencePriorities2App')
 
 }]);
 
-angular.module('sciencePriorities2App').controller("CenterController" , ["$scope",function($scope){
+angular.module('sciencePriorities2App').controller("CenterController" , ["$scope","$compile","$templateCache",function($scope, $compile, $templateCache){
 	$scope.multiViewModel="single"
 	$scope.dropped= function (dragEl, dropEl ){
-		//console.log("Object "+dragEl+" Droped on " + dropEl);
-		drag= $("#"+dragEl);
-		createBarchart(dropEl,"test","test");
-	}
+		var drag= $("#"+dragEl);
+		var dropDiv = $("#"+dropEl+ " > div");
+		
+		// pass drag parameter to associated controller scope
+		console.log(drag[0].attributes.selectedEntity);
+		angular.element(dropDiv[0]).scope().selectedEntity= drag[0].attributes.selectedEntity.value;
+		angular.element(dropDiv[0]).scope().selectedProperty= drag[0].attributes.selectedProperty.value;
+			
+		// load select benchmark template
+		$.get("app/benchmarkMenu/benchmarkMenu.html", function(htmlFile) {	
+		
+			//angular.element(dropDiv[0]).scope().HTMLplace= htmlFile;
+			var $el= $("#"+dropEl+ " > div").empty().append(htmlFile);
+			// compile the code to angular scope so call methods works properly
+			$compile($el)(angular.element(dropDiv[0]).scope());
+			
+			// jquery code which is not acceptable in angular
+			//$("#"+dropEl+ " > div").empty().append(htmlFile);
+		});
+	};
 }]);
+
+angular.module('sciencePriorities2App').controller("layoutController" , ["$scope",function($scope){
+	// handle d3 animation
+	$scope.layoutTypeClicked = function(selectedLayout){
+		//console.log("btn click: "+ selectedLayout + "pro: "+$scope.selectedProperty+"ent: "+$scope.selectedEntity);
+	};
+}]);
+
+
