5e1e47705ac375dbe4b5592e6d1c7b87777d1cd4
Arash Khosravi <arashsoft@gmail.com>
2014-11-14 16:00:59 -0500

    remove console.log for query results

--- server/makeJson.js
+++ server/makeJson.js
@@ -16,7 +16,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Awards|Universities|pieChart":
 			connection.query("select U.Short_Name as name, sum(A.Amount) as value from award as A join department as D on A.Department=D.ID join faculty as F on D.Faculty=F.ID join university as U on F.University=U.ID where A.ID <=20074 GROUP BY U.Long_Name", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -24,7 +23,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Awards|Departments|pieChart":
 			connection.query("select D.Name as name, sum(A.Amount) as value from award as A join department as D on A.Department=D.ID join faculty as F on D.Faculty=F.ID join university as U on F.University=U.ID where A.ID > 20074 and U.Short_Name='UWO'  GROUP BY A.Department", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -32,7 +30,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Awards|Top Sponsors|pieChart":
 			connection.query("select S.Name as name, sum(A.Amount) as value from award as A join program as P on A.Program = P.ID join sponsor as S on P.Sponsor = S.ID where A.ID > 20074 GROUP BY S.Name ORDER BY value DESC limit 10", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -40,7 +37,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Awards|Source of funding|pieChart":
 			connection.query("select SOF.sponsor_type as name, sum(A.Amount) as value from award as A join program as P on A.Program = P.ID join sponsor as S on P.Sponsor = S.ID join source_of_funding as SOF on S.Source=SOF.ID GROUP BY SOF.sponsor_type", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -48,7 +44,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Professors|Research chair|pieChart":
 			connection.query("SELECT table1.type as name , count(*) as value FROM (select RCT.type from professor as P join research_chair as RC on P.ResearchChair = RC.ID join research_chair_type as RCT on RC.type = RCT.ID) as table1 GROUP BY type", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -56,7 +51,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Professors|Department|pieChart":
 			connection.query("SELECT table1.type as name , count(*) as value FROM ( select D.Name as type from professor as P join department as D on P.Department_Primary=D.ID join faculty as F on D.Faculty=F.ID join university as U on F.University=U.ID where F.Name='Faculty of Science' and U.Short_Name='UWO' ) as table1 GROUP BY type", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
@@ -64,7 +58,6 @@ exports.makeJson = function(entityName, propertyName, layoutName, callback){
 		case "Professors|Rank|pieChart":
 			connection.query("SELECT table1.type as name , count(*) as value FROM ( select P.Rank as type from professor as P join department as D on P.Department_Primary=D.ID join faculty as F on D.Faculty=F.ID join university as U on F.University=U.ID where F.Name='Faculty of Science' and U.Short_Name='UWO' and  P.Rank != 'null' ) as table1 GROUP BY type", function(err, rows, fields) {
 				connection.end();
-				console.log(rows);
 				callback(rows);
 			});
 			break;
