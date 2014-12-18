5d8f21d137010904b747e4835ec89cf721db9dee
Arash <arashsoft@gmail.com>
2014-12-18 11:50:41 -0500

    Move authentication logic to separate module

---
+++ server/authenticate.js
@@ -0,0 +1,57 @@
+// handle authentication methods - this module use passport module
+
+var LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+
+// set users:
+// TODO: read users from database
+var users = [
+  { id: 1, username: 'bob', password: 'secret' },
+  { id: 2, username: 'joe', password: 'birthday'}
+];
+
+passport.use('local-login', new LocalStrategy({passReqToCallback : true }, function(req, username, password, done) {
+	findByUsername( username , function (err, user) {
+			if (err) { return done(err); }
+			if (!user) {
+				return done(null, false,  req.flash('loginMessage', 'Incorrect username') );
+			}
+			if (user.password != password) {
+				return done(null, false,  req.flash('loginMessage', 'Incorrect password')  );
+			}
+			return done(null, user);
+	 });
+}));
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+	 findById(id, function(err, user) {
+			done(err, user);
+	 });
+});
+
+module.exports = passport;
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+	
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username == username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
--- server/routes.js
+++ server/routes.js
@@ -1,66 +1,10 @@
 /**
  * Main application routes
  */
-
 'use strict';
-// set users:
-
-var users = [
-  { id: 1, username: 'bob', password: 'secret' },
-  { id: 2, username: 'joe', password: 'birthday'}
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-	
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username == username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
 var errors = require('./components/errors');
-var jsonHandler = require("./makeJson.js")
-
-// authentication
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use('local-login', new LocalStrategy({passReqToCallback : true }, function(req, username, password, done) {
-	
-	findByUsername( username , function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false,  req.flash('loginMessage', 'Incorrect username') );
-      }
-      if (user.password != password) {
-        return done(null, false,  req.flash('loginMessage', 'Incorrect password')  );
-      }
-      return done(null, user);
-   });
-}));
-
-passport.serializeUser(function(user, done) {
-	done(null, user.id);
-});
-
-// used to deserialize the user
-passport.deserializeUser(function(id, done) {
-   findById(id, function(err, user) {
-      done(err, user);
-   });
-});
+var jsonHandler = require('./makeJson.js');
+var passport = require('./authenticate.js');
 
 module.exports = function(app) {
 
@@ -75,6 +19,9 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
+	//login request send false login to / and correct login to /main
+	app.post('/', passport.authenticate('local-login', { successRedirect: '/main', failureRedirect: '/', failureFlash: true }));
+	
 	// json request
 	app.get('/jsonrequest/:entityName/:propertyName/:layoutName',function(req, res) {
 		if (req.isAuthenticated()){
@@ -97,9 +44,6 @@ module.exports = function(app) {
 		 we should use callback function instead of return value; */
 		//res.send(jsonHandler.makeJson(req.params.entityName,req.params.propertyName));
    });
-
-	//login request
-	app.post('/', passport.authenticate('local-login', { successRedirect: '/main', failureRedirect: '/', failureFlash: true }));
   
   // main page
 	app.get('/main' , function(req, res) {
