b4eb4d73353efb59eb7cd1b422b048a48509bf1e
Arash <arashsoft@gmail.com>
2015-02-19 13:46:00 -0500

    abandon professorsRelation module

--- client/app/modules/matrixLink/professorsRelation.js
+++ client/app/modules/matrixLink/professorsRelation.js
@@ -1,3 +1,5 @@
+// abandoned!!
+
 // matrixLink and compareDepartments call this function but it can be use individually.
 // unlike other modules, parrentDiv should be jquery object not the object ID
 // departmentColor is optional
